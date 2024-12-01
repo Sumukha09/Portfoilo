f277d78fe865e21f45ca0d7ad98d0a36d4ebc9ca
Sumukha P <sumukhapb2004@gmail.com>
2024-12-01 20:30:38 +0530

    final touches

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -10,6 +10,7 @@ import {
   FormLabel,
   FormMessage,
 } from "./ui/form"
+import { useState } from 'react'
 
 const formSchema = z.object({
   fullname: z.string().min(2, {
@@ -21,6 +22,7 @@ const formSchema = z.object({
 })
 
 const AuthForm = ({ type }) => {
+  const [isloading, setisLoading] = useState(false)
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -38,7 +40,7 @@ const AuthForm = ({ type }) => {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} 
         className="auth-form flex flex-col gap-5">
-          <h1 className='form-title text-2xl font-bold text-white mb-8'>
+          <h1 className=' text-2xl font-bold text-white mb-8'>
             {type === "signup" ? "Sign Up" : "Sign In"}
           </h1>
           {type === "signup" && 
@@ -82,8 +84,14 @@ const AuthForm = ({ type }) => {
             )}
           />
           
-          <button className=" mt-8 w-full bg-white text-black py-4 rounded-2xl text-lg font-medium ">
-            {type === "signin" ? "Sign In" : "Create Account"}
+          <button className="mt-8 w-full bg-white text-black py-4 rounded-2xl text-lg font-medium flex items-center justify-center">
+            {type === "signin" ? "Sign In" : "Sign Up"}
+            {isloading && 
+              <svg className="animate-spin h-5 w-5 ml-2" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+            }
           </button>
 
            <p className="text-white/60 text-center text-base mt-4">
@@ -93,7 +101,7 @@ const AuthForm = ({ type }) => {
             }
             <Link
               to={type === "signup" ? "/signin" : "/signup"}
-              className="text-white "
+              className="text-white"
             > 
               {type === "signup" ? "Login" : "Sign up"}
             </Link>
